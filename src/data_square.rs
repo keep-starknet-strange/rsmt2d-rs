a4be42cfa1aad341927a7016d3e3f303cc13c976
Abdelhamid Bakhta <abdelhamid.bakhta@gmail.com>
2022-11-28 16:33:16 +0100

    🚧 pp pass

--- src/data_square.rs
+++ src/data_square.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use crate::{merkle_tree::MerkleTree, Matrix2D, Matrix3D};
 use eyre::{eyre, Result};
 
@@ -14,10 +16,26 @@ pub struct DataSquare {
 }
 
 impl DataSquare {
+    /// Create a new data square.
+    /// Data must be a square matrix.
+    /// Data must not be empty.
+    ///
+    /// # Arguments
+    ///
+    /// * `original_data` - Original data.
+    /// * `merkle_tree` - `MerkleTree`.
+    ///
+    /// # Returns
+    ///
+    /// * `DataSquare` - Data square.
+    /// * `Result` - Error.
     pub fn new(original_data: Matrix2D, _merkle_tree: &dyn MerkleTree) -> Result<Self> {
-        if original_data.len() == 0 {
+        // Check that data is not empty.
+        if original_data.is_empty() {
             return Err(eyre!("Data must not be empty."));
         }
+
+        // Check that data is a square matrix.
         let data_len = f64::from(original_data.len() as u32);
         let width = data_len.sqrt();
         if width * width != data_len {
@@ -25,23 +43,35 @@ impl DataSquare {
         }
         let width: usize = width as usize;
 
-        let chunck_size = original_data[0].len();
+        // Check that chunks are all the same size.
+        let chunk_size = original_data[0].len();
         for row in original_data.iter() {
-            if row.len() != chunck_size {
+            if row.len() != chunk_size {
                 return Err(eyre!("All chunks must be of equal size."));
             }
         }
+
+        // Create row-major data squares.
         let mut square_row: Matrix3D = vec![vec![]; width];
         for i in 0..width {
             square_row[i] = original_data[i * width..i * width + width].to_vec();
         }
+
+        // Create column-major data squares.
+        let mut square_col: Matrix3D = vec![vec![]; width];
+        for j in 0..width {
+            for i in 0..width {
+                square_col[j].push(original_data[i * width + j].to_vec());
+            }
+        }
+
         Ok(Self {
-            square_row: Matrix3D::default(),
-            square_col: Matrix3D::default(),
-            width: width as usize,
-            chunk_size: 0,
-            row_roots: vec![vec![0; original_data[0].len()]; original_data.len()],
-            col_roots: vec![vec![0; original_data[0].len()]; original_data.len()],
+            square_row,
+            square_col,
+            width,
+            chunk_size,
+            row_roots: vec![],
+            col_roots: vec![],
         })
     }
 }
--- tests/data_square.rs
+++ tests/data_square.rs
@@ -11,11 +11,41 @@ mod tests {
             vec![0, 1, 2, 3],
             vec![4, 5, 6, 7],
             vec![8, 9, 10, 11],
-            vec![8, 9, 10, 11],
+            vec![12, 13, 14, 15],
         ];
-        let _data_square = DataSquare::new(data, merkle_tree.as_ref())?;
+        let data_square = DataSquare::new(data, merkle_tree.as_ref())?;
+        assert!(data_square.chunk_size == 4);
+        assert!(data_square.width == 2);
+        assert!(
+            data_square.square_row
+                == vec![
+                    vec![vec![0, 1, 2, 3], vec![4, 5, 6, 7]],
+                    vec![vec![8, 9, 10, 11], vec![12, 13, 14, 15]],
+                ]
+        );
+        assert!(
+            data_square.square_col
+                == vec![
+                    vec![vec![0, 1, 2, 3], vec![8, 9, 10, 11]],
+                    vec![vec![4, 5, 6, 7], vec![12, 13, 14, 15],]
+                ]
+        );
 
         Ok(())
+    }
+
+    #[test]
+    fn when_data_is_empty_then_it_fails() {
+        let merkle_tree = merkle_tree::new();
+
+        let data: Matrix2D = vec![];
+        let result = DataSquare::new(data, merkle_tree.as_ref());
+
+        assert!(result.is_err());
+        assert!(result
+            .unwrap_err()
+            .to_string()
+            .contains("Data must not be empty."));
     }
 
     #[test]
@@ -29,6 +59,6 @@ mod tests {
         assert!(result
             .unwrap_err()
             .to_string()
-            .contains("Number of chunks must be a square number"));
+            .contains("Number of chunks must be a square number."));
     }
 }
