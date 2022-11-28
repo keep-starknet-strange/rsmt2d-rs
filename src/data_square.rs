bb594adac57d902786cd7d42aa19094d142a9c4f
LucasLvy <lucaslevy10@gmail.com>
2022-11-28 15:15:41 +0100

    WIP

--- src/data_square.rs
+++ src/data_square.rs
@@ -15,10 +15,25 @@ pub struct DataSquare {
 
 impl DataSquare {
     pub fn new(original_data: Matrix2D, _merkle_tree: &dyn MerkleTree) -> Result<Self> {
+        if original_data.len() == 0 {
+            return Err(eyre!("Data must not be empty."));
+        }
         let data_len = f64::from(original_data.len() as u32);
         let width = data_len.sqrt();
         if width * width != data_len {
-            return Err(eyre!("Number of chunks must be a square number"));
+            return Err(eyre!("Number of chunks must be a square number."));
+        }
+        let width: usize = width as usize;
+
+        let chunck_size = original_data[0].len();
+        for row in original_data.iter() {
+            if row.len() != chunck_size {
+                return Err(eyre!("All chunks must be of equal size."));
+            }
+        }
+        let mut square_row: Matrix3D = vec![vec![]; width];
+        for i in 0..width {
+            square_row[i] = original_data[i * width..i * width + width].to_vec();
         }
         Ok(Self {
             square_row: Matrix3D::default(),
