ad994f0a38984236e5460c61533e93e4061ba8dc
LucasLvy <lucaslevy10@gmail.com>
2022-11-28 14:30:09 +0100

    WIP

--- src/data_square.rs
+++ src/data_square.rs
@@ -1,4 +1,5 @@
-use crate::{Matrix2D, Matrix3D};
+use crate::{merkle_tree::MerkleTree, Matrix2D, Matrix3D};
+use eyre::{eyre, Result};
 
 /// Store all data for an original data square (ODS) or extended data square (EDS).
 /// Data is duplicated in both row-major and column-major order in order to be able to provide zero-allocation column slices.
@@ -9,6 +10,17 @@ pub struct DataSquare<'a> {
     pub chunk_size: usize,
     pub row_roots: &'a Matrix2D<'a>,
     pub col_roots: &'a Matrix2D<'a>,
+}
+
+impl<'a> DataSquare<'a> {
+    pub fn new(original_data: Matrix2D, merkle_tree: &dyn MerkleTree) -> Result<Self> {
+        let data_len = f64::from(original_data.len() as u32);
+        let width = data_len.sqrt();
+        if width * width != data_len {
+            return Err(eyre!("Number of chunks must be a square number"));
+        }
+        todo!()
+    }
 }
 
 /// Represent an extended piece of data.
---
+++ tests/data_square.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        let data_square = DataSquare::new();
+    }
+}
